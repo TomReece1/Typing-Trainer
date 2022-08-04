54761fdb758aed226f2e486e50fb23b5d7443370
Tom Reece <tomreece@toms-mbp.home>
2022-08-04 16:25:48 +0100

    make react app with typescript

--- front-end/typing-trainer/src/App.tsx
+++ front-end/typing-trainer/src/App.tsx
@@ -1,10 +1,12 @@
-import "./App.css";
+import React from 'react';
+
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Typing Trainer</h1>
+<h1>Typing Trainer</h1>
       </header>
     </div>
   );
